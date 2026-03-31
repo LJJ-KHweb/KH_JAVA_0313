50802f65df98cae7ad5a166560f95a87f2d8aa86
LJJ_KHweb <blue7257@naver.com>
2026-03-31 17:50:47 +0900

    추가

---
+++ 12_Network/src/com/kh/network/run/Run.java
@@ -0,0 +1,24 @@
+package com.kh.network.run;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+// 	네트워크
+// 	통신 가능한 두 대 이상의 장치들을 물리적 또는 논리적으로 연결한 환경
+//	목적 : 데이터의 교환
+//	통신이 가능한 장치 == Host 라고 통칭
+//	종단간의 실체 통신 장치는 EndPoint라고 표현
+//	IP(Internet Protocol) Address / MAC(Media Access Controller) Address
+//	-MAC Address : NIC(Network Interface Card)에 제조시 붙여지는 물리적인 주소
+//	-IP Address  : Internet 상에서 각 Host 들을 식별할 수 있는 논리적인 주소
+//	-Port Number : Host 내부에서 작동하는 process 를 식별할 때 사용하는 내부 번호
+//	
+//
+//
+//
+//
